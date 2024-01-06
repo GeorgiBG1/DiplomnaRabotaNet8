d22c0bf3814f4682987a65bfc1cc458ad78a87de
Georgi <georgegalinov@gmail.com>
2024-01-06 12:10:35 +0200

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using DiplomnaRabotaNet8.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -17,7 +18,7 @@ namespace DiplomnaRabotaNet8.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
